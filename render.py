ce37874eab6ddaef840ee3d62ba0b96cf85927bd
Mark Ferry <mark@markferry.net>
2022-10-12 15:54:52 +0100

    render.py: smush the selected zone into a "zone" key
    This means each template can be passed a unified config.json.
    The separate players.json and $zone.json can go away.

---
+++ render.py
@@ -0,0 +1,49 @@
+#!/bin/env python
+import os
+import sys
+import argparse
+import chevron
+import json
+
+
+def parse_args():
+    def is_file_or_pipe(arg):
+        if not os.path.exists(arg) or os.path.isdir(arg):
+            parser.error("The file {0} does not exist!".format(arg))
+        else:
+            return arg
+
+    parser = argparse.ArgumentParser(description="Render templates")
+    parser.add_argument(
+        "-d",
+        "--data",
+        dest="data",
+        help="The json data file",
+        type=is_file_or_pipe,
+        default={},
+    )
+    parser.add_argument(
+        "-z",
+        "--zone",
+        dest="zone",
+        required=False,
+        help="The zone name",
+        default={},
+    )
+    parser.add_argument("template", metavar="template", help="The mustache file")
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+
+    with open(args.data, "r") as j:
+        d = json.load(j)
+        if args.zone:
+            d["zone"] = d["hosts"][args.zone]
+        with open(args.template, "r") as t:
+            print(chevron.render(t, d))
+
+
+if __name__ == "__main__":
+    main()
